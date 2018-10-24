e86266e5c902bb8c1022573f4bd937b0df8f10da
GalGog <galynna00@gmail.com>
2018-10-24 15:13:34 +0100

    Taking Turns

--- src/index.js
+++ src/index.js
@@ -9,35 +9,29 @@ import * as serviceWorker from './serviceWorker';
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
-class Square extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-    render (){
-        return (
+ function Square(props) {
+     return (
             <button
                 className="square"
-                onClick={ ()=>  this.setState({value: 'X'})}
-            >
-                {this.state.value}
+                onClick={props.onClick}>
+                {props.value}
             </button>
         );
-    }
 }
 class Board extends React.Component {
     constructor(props){
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
     handleClick(i){
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState ({squares:squares});
+        squares[i] = this.state.xIsNext ? 'X': '0';
+        this.setState ({squares:squares,
+        xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
@@ -47,7 +41,7 @@ class Board extends React.Component {
     );
     }
     render (){
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X': '0');
 
     return(
         <div>
