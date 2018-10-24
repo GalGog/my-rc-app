d2dd6b34ad8834e329b12a3b61faead729792f79
GalGog <galynna00@gmail.com>
2018-10-24 16:39:24 +0100

    Taking Turns

--- src/index.js
+++ src/index.js
@@ -28,6 +28,9 @@ class Board extends React.Component {
     }
     handleClick(i){
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X': '0';
         this.setState ({squares:squares,
         xIsNext: !this.state.xIsNext,
@@ -41,7 +44,13 @@ class Board extends React.Component {
     );
     }
     render (){
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X': '0');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner){
+        status = 'Winner:' + winner;
+    } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : '0');
+    }
 
     return(
         <div>
@@ -67,6 +76,16 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component{
+     constructor(props){
+         super(props);
+         this.state = {
+             history: [{
+                 squares: Array(9).fill(null),
+             }],
+             xIsNext: true,
+         };
+     }
+
     render(){
         return(
             <div className="game">
@@ -87,3 +106,23 @@ ReactDOM.render(
 <Wrapper />,
     document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
